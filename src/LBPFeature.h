07e6c543782ea4407f6a3393bfbefa7930fe4a10
gcielniak <grzegorz.cielniak@gmail.com>
2013-07-03 19:27:30 +0100

    LBP border handling
    fixed the padded version of the border handling

--- src/LBPFeature.h
+++ src/LBPFeature.h
@@ -9,11 +9,17 @@ namespace LinLib
 		BORDER_NAN
 	};
 
+	/// class implementing Local Binary Pattern feature
+	/// supports:
+	/// - NaN values specified as a specific index value
+	/// - two different ways of handling borders: either as NaN values or padded with the nearest pixel
+	/// - visualisation of the features in form of an image
 	class LBPFeature
 	{
 		cv::Mat feature, feature_image, feature_pyr;
 		BorderType border_type;
 
+		/// calculate lbp for uchar, used by the border filling procedure
 		inline int CalcIndex(uchar p, uchar pa, uchar pb, uchar pc, uchar pd, uchar pe, uchar pf, uchar pg, uchar ph, int nan_value)
 		{
 			uchar a = 1, b = 2, c = 4, d = 8, e = 16, f = 32, g = 64, h = 128;
@@ -38,6 +44,7 @@ namespace LinLib
 			return (a | b | c | d | e | f | g | h);
 		}
 
+		/// calculate lbp for ushort, used by the border filling procedure
 		inline int CalcIndex(ushort p, ushort pa, ushort pb, ushort pc, ushort pd, ushort pe, ushort pf, ushort pg, ushort ph, int nan_value)
 		{
 			uchar a = 1, b = 2, c = 4, d = 8, e = 16, f = 32, g = 64, h = 128;
@@ -62,6 +69,7 @@ namespace LinLib
 			return (a | b | c | d | e | f | g | h);
 		}
 
+		/// calculate lbp feature, with the specified NaN value (-1 = ignore)
 		cv::Mat& Calculate(const cv::Mat &_image, int nan_value=-1)
 		{
 			uchar p, *_p, *_p1, *_p2;
@@ -165,8 +173,6 @@ namespace LinLib
 			}
 
 			//handle borders
-			//current implementation does not handle 0 as separate values from NaN
-			//solution: replace zeros with equivalent padding pixels
 			switch (border_type)
 			{
 			case BORDER_PADD:
@@ -174,25 +180,27 @@ namespace LinLib
 
 				if (image.type() == CV_8UC1)
 				{
-					//first row
+					//first row: e p a, e p a, d c b
 					_p = image.ptr<uchar>(0);
 					_p2 = image.ptr<uchar>(1);
 					for(int j = 1; j < image.cols-1; j++)
-						_pd[CalcIndex(_p[j], _p[j+1], _p2[j+1], _p2[j], _p2[j-1], _p[j-1], 0, 0, 0, nan_value)]++;
+						_pd[CalcIndex(_p[j], _p[j+1], _p2[j+1], _p2[j], _p2[j-1], _p[j-1], _p[j-1], _p[j], _p[j+1], nan_value)]++;
 
-					//top corner points
-					_pd[CalcIndex(_p[0], _p[1], _p2[1], _p2[0], 0, 0, 0, 0, 0, nan_value)]++;
-					_pd[CalcIndex(_p[image.cols-1], 0, 0, _p2[image.cols-1], _p2[image.cols-2], _p[image.cols-2], 0, 0, 0, nan_value)]++;
+					//top corner points: p p a, p p a, c c b
+					_pd[CalcIndex(_p[0], _p[1], _p2[1], _p2[0], _p2[0], _p[0], _p[0], _p[0], _p[1], nan_value)]++;
+					// e p p, e p p, d c c
+					_pd[CalcIndex(_p[image.cols-1], _p[image.cols-1], _p2[image.cols-1], _p2[image.cols-1], _p2[image.cols-2], _p[image.cols-2], _p[image.cols-2], _p[image.cols-1], _p[image.cols-1], nan_value)]++;
 
-					//last row
+					//last row: f g h, e p a, e p a
 					_p1 = image.ptr<uchar>(image.rows-2);
 					_p = image.ptr<uchar>(image.rows-1);
 					for(int j = 1; j < image.cols-1; j++)
-						_pd[CalcIndex(_p[j], _p[j+1], 0, 0, 0, _p[j-1], _p1[j-1], _p1[j], _p1[j+1], nan_value)]++;
+						_pd[CalcIndex(_p[j], _p[j+1], _p[j+1], _p[j], _p[j-1], _p[j-1], _p1[j-1], _p1[j], _p1[j+1], nan_value)]++;
 
-					//bottom corner points
-					_pd[CalcIndex(_p[0], _p[1], 0, 0, 0, 0, 0, _p1[0], _p1[1], nan_value)]++;
-					_pd[CalcIndex(_p[image.cols-1], 0, 0, 0, 0, _p[image.cols-2], _p1[image.cols-2], _p1[image.cols-1], 0, nan_value)]++;
+					//bottom corner points: g g h, p p a, p p a
+					_pd[CalcIndex(_p[0], _p[1], _p[1], _p[0], _p[0], _p[0], _p1[0], _p1[0], _p1[1], nan_value)]++;
+					// f g g, e p p, e p p
+					_pd[CalcIndex(_p[image.cols-1], _p[image.cols-1], _p[image.cols-1], _p[image.cols-1], _p[image.cols-2], _p[image.cols-2], _p1[image.cols-2], _p1[image.cols-1], _p1[image.cols-1], nan_value)]++;
 
 					//first and last column 
 					for(int i = 1; i < image.rows-1; i++)
@@ -200,31 +208,35 @@ namespace LinLib
 						_p = image.ptr<uchar>(i);
 						_p1 = image.ptr<uchar>(i-1);
 						_p2 = image.ptr<uchar>(i+1);
-						_pd[CalcIndex(_p[0], _p[1], _p2[1], _p2[0], 0, 0, 0, _p1[0], _p1[1], nan_value)]++;
-						_pd[CalcIndex(_p[image.cols-1], 0, 0, _p2[image.cols-1], _p2[image.cols-2], _p[image.cols-2], _p1[image.cols-2], _p1[image.cols-1], 0, nan_value)]++;
+						// g g h, p p a, c c b
+						_pd[CalcIndex(_p[0], _p[1], _p2[1], _p2[0], _p2[0], _p[0], _p1[0], _p1[0], _p1[1], nan_value)]++;
+						// f g g, e p p, d c c
+						_pd[CalcIndex(_p[image.cols-1], _p[image.cols-1], _p2[image.cols-1], _p2[image.cols-1], _p2[image.cols-2], _p[image.cols-2], _p1[image.cols-2], _p1[image.cols-1], _p1[image.cols-1], nan_value)]++;
 					}
 				}
 				else if (image.type() == CV_16UC1)
 				{
-					//first row
+					//first row: e p a, e p a, d c b
 					_pp = image.ptr<ushort>(0);
 					_pp2 = image.ptr<ushort>(1);
 					for(int j = 1; j < image.cols-1; j++)
-						_pd[CalcIndex(_pp[j], _pp[j+1], _pp2[j+1], _pp2[j], _pp2[j-1], _pp[j-1], 0, 0, 0, nan_value)]++;
+						_pd[CalcIndex(_pp[j], _pp[j+1], _pp2[j+1], _pp2[j], _pp2[j-1], _pp[j-1], _pp[j-1], _pp[j], _pp[j+1], nan_value)]++;
 
-					//top corner points
-					_pd[CalcIndex(_pp[0], _pp[1], _pp2[1], _pp2[0], 0, 0, 0, 0, 0, nan_value)]++;
-					_pd[CalcIndex(_pp[image.cols-1], 0, 0, _pp2[image.cols-1], _pp2[image.cols-2], _pp[image.cols-2], 0, 0, 0, nan_value)]++;
+					//top corner points: p p a, p p a, c c b
+					_pd[CalcIndex(_pp[0], _pp[1], _pp2[1], _pp2[0], _pp2[0], _pp[0], _pp[0], _pp[0], _pp[1], nan_value)]++;
+					// e p p, e p p, d c c
+					_pd[CalcIndex(_pp[image.cols-1], _pp[image.cols-1], _pp2[image.cols-1], _pp2[image.cols-1], _pp2[image.cols-2], _pp[image.cols-2], _pp[image.cols-2], _pp[image.cols-1], _pp[image.cols-1], nan_value)]++;
 
-					//last row
+					//last row: f g h, e p a, e p a
 					_pp1 = image.ptr<ushort>(image.rows-2);
 					_pp = image.ptr<ushort>(image.rows-1);
 					for(int j = 1; j < image.cols-1; j++)
-						_pd[CalcIndex(_pp[j], _pp[j+1], 0, 0, 0, _pp[j-1], _pp1[j-1], _pp1[j], _pp1[j+1], nan_value)]++;
+						_pd[CalcIndex(_pp[j], _pp[j+1], _pp[j+1], _pp[j], _pp[j-1], _pp[j-1], _pp1[j-1], _pp1[j], _pp1[j+1], nan_value)]++;
 
-					//bottom corner points
-					_pd[CalcIndex(_pp[0], _pp[1], 0, 0, 0, 0, 0, _pp1[0], _pp1[1], nan_value)]++;
-					_pd[CalcIndex(_pp[image.cols-1], 0, 0, 0, 0, _pp[image.cols-2], _pp1[image.cols-2], _pp1[image.cols-1], 0, nan_value)]++;
+					//bottom corner points: g g h, p p a, p p a
+					_pd[CalcIndex(_pp[0], _pp[1], _pp[1], _pp[0], _pp[0], _pp[0], _pp1[0], _pp1[0], _pp1[1], nan_value)]++;
+					// f g g, e p p, e p p
+					_pd[CalcIndex(_pp[image.cols-1], _pp[image.cols-1], _pp[image.cols-1], _pp[image.cols-1], _pp[image.cols-2], _pp[image.cols-2], _pp1[image.cols-2], _pp1[image.cols-1], _pp1[image.cols-1], nan_value)]++;
 
 					//first and last column 
 					for(int i = 1; i < image.rows-1; i++)
@@ -232,8 +244,10 @@ namespace LinLib
 						_pp = image.ptr<ushort>(i);
 						_pp1 = image.ptr<ushort>(i-1);
 						_pp2 = image.ptr<ushort>(i+1);
-						_pd[CalcIndex(_pp[0], _pp[1], _pp2[1], _pp2[0], 0, 0, 0, _pp1[0], _pp1[1], nan_value)]++;
-						_pd[CalcIndex(_pp[image.cols-1], 0, 0, _pp2[image.cols-1], _pp2[image.cols-2], _pp[image.cols-2], _pp1[image.cols-2], _pp1[image.cols-1], 0, nan_value)]++;
+						// g g h, p p a, c c b
+						_pd[CalcIndex(_pp[0], _pp[1], _pp2[1], _pp2[0], _pp2[0], _pp[0], _pp1[0], _pp1[0], _pp1[1], nan_value)]++;
+						// f g g, e p p, d c c
+						_pd[CalcIndex(_pp[image.cols-1], _pp[image.cols-1], _pp2[image.cols-1], _pp2[image.cols-1], _pp2[image.cols-2], _pp[image.cols-2], _pp1[image.cols-2], _pp1[image.cols-1], _pp1[image.cols-1], nan_value)]++;
 					}
 				}
 				break;
@@ -249,19 +263,23 @@ namespace LinLib
 		}
 
 	public:
+		/// Constructor
 		LBPFeature()
 			: border_type(BORDER_NAN) {}
 
+		/// Set border type
 		void BorderHandling(BorderType type)
 		{
 			border_type = type;
 		}
 
+		/// Get border type 
 		BorderType BorderHandling()
 		{
 			return border_type;
 		}
 
+		/// Get the feature with a specified pyramid depth (def: 0) and NaN value (def -1 = no NaN)
 		cv::Mat& Get(const cv::Mat &_image, int depth=0, int nan_value=-1)
 		{
 			feature = Calculate(_image, nan_value);
@@ -284,6 +302,7 @@ namespace LinLib
 			return feature;
 		}
 
+		/// Visualise features as an image
 		cv::Mat& GetFeatureImage(const cv::Mat &_image)
 		{
 			uchar p, *_p, *_p1, *_p2;
